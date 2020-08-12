e7fdc217583cee0ac13d8ddcd615e4822a0172c8
msc24x <msc24x@gmail.com>
2020-08-12 17:32:38 +0530

    Add splash screen

---
+++ app/src/main/java/com/lscambo13/contactlessForWhatsApp/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.lscambo13.contactlessForWhatsApp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
