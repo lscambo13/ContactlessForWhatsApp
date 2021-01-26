b0fd8568d3342bd99b75a509084c5fa9312f1240
lscambo13 <ls.cambo13@gmail.com>
2021-01-26 18:57:39 +0530

    Begin work on version 2.1
    Signed-off-by: lscambo13 <ls.cambo13@gmail.com>

--- app/build/generated/source/buildConfig/debug/com/lscambo13/contactlessForWhatsApp/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/lscambo13/contactlessForWhatsApp/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.lscambo13.contactlessForWhatsApp";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 6;
-  public static final String VERSION_NAME = "2.0";
+  public static final int VERSION_CODE = 7;
+  public static final String VERSION_NAME = "2.1";
 }
