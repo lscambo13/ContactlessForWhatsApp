b05d1056419b3f0576a88abcc07aa7de92873729
lscambo13 <ls.cambo13@gmail.com>
2021-02-01 20:02:45 +0530

    Fix ripple effects in night mode Fix exit button in three-dot menu Fix easter egg Update changelog
    Signed-off-by: lscambo13 <ls.cambo13@gmail.com>

--- app/src/main/java/com/lscambo13/contactlessForWhatsApp/Preferences.kt
+++ app/src/main/java/com/lscambo13/contactlessForWhatsApp/Preferences.kt
@@ -6,17 +6,13 @@ import androidx.appcompat.app.AppCompatDelegate
 
 class Preferences(context: Context) {
 
+//    Theme settings begin. --------------------------------
+
     // This is the save file for theme settings.
     val preferenceThemeFile = "ConfigTheme"
 
     // This is the integer entry for theme style.
     val preferenceIDTheme = "CurrentTheme"
-
-    // This is the save file for detecting first run.
-    val preferenceFirstRunFile = "ConfigFirstRun"
-
-    // This is the integer entry for first run.
-    val preferenceIDFirstRun = "FirstRun"
 
     // This reads the theme file, ConfigTheme,
     // and reads the presently set theme style from CurrentTheme. (default = set by system)
@@ -32,6 +28,24 @@ class Preferences(context: Context) {
         return preferenceTheme.getInt(preferenceIDTheme, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 
+    // This saves the set theme as -1, 1 or 2.
+    fun setCurrentTheme(theme: Int) {
+        val editor = preferenceTheme.edit()
+        editor.putInt(preferenceIDTheme, theme)
+        editor.apply()
+    }
+
+//  Theme settings end. --------------------------------
+
+
+//  First run settings begin. --------------------------
+
+    // This is the save file for detecting first run.
+    val preferenceFirstRunFile = "ConfigFirstRun"
+
+    // This is the integer entry for first run.
+    val preferenceIDFirstRun = "FirstRun"
+
     // This reads the first run detection file, ConfigFirstRun,
     // and detects if the app is installed freshly (Int 0) or updated (Int !0) from entry FirstRun.
     val preferenceFirstRun: SharedPreferences =
@@ -41,17 +55,40 @@ class Preferences(context: Context) {
         return preferenceFirstRun.getInt(preferenceIDFirstRun, 0)
     }
 
-    // This saves the set theme as -1, 1 or 2.
-    fun setCurrentTheme(theme: Int) {
-        val editor = preferenceTheme.edit()
-        editor.putInt(preferenceIDTheme, theme)
-        editor.apply()
-    }
-
     // This saves the first run property as 0 or VersionCode.
     fun setFirstRun(run: Int) {
         val editor = preferenceFirstRun.edit()
         editor.putInt(preferenceIDFirstRun, run)
         editor.apply()
     }
+
+//  First run settings end. --------------------------
+
+
+//  Easter egg settings begin. -----------------------
+
+    // This is the save file for collecting easter egg.
+    val preferenceEasterEggFile = "ConfigEasterEgg"
+
+    // This is the integer entry for easter egg.
+    val preferenceIDEasterEgg = "EasterEgg"
+
+    // This reads the easter egg collection file, ConfigEasterEgg,
+    // and detects if the egg is collected (true) or not (false) from entry EasterEgg.
+    val preferenceEasterEgg: SharedPreferences =
+        context.getSharedPreferences(preferenceEasterEggFile, Context.MODE_PRIVATE)
+
+    fun getEasterEgg(): Boolean {
+        return preferenceEasterEgg.getBoolean(preferenceIDEasterEgg, false)
+    }
+
+    // This saves the easter egg property as true or false.
+    fun setEasterEgg(collected: Boolean) {
+        val editor = preferenceEasterEgg.edit()
+        editor.putBoolean(preferenceIDEasterEgg, collected)
+        editor.apply()
+    }
+
+//  Easter egg settings end. -----------------------
+
 }
